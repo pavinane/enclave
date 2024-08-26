4702c40b8790b3c89e5739db790976b14ab5c9e0
pavinane <pavimegan77@gmail.com>
2024-08-26 23:48:06 +0530

    add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import "./App.css";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Home from "./component/Home/Home";
+import About from "./component/About/About";
+import Header from "./component/Header/Header";
+
+const AppLayout = () => (
+  <>
+    <Header />
+    <Outlet />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      { path: "/about", element: <About /> },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <h1>hii pavi</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div className=" text-3xl text-red-500 underline">Welcome Home</div>;
+}
+
+export default Home;
