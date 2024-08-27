d9514e9ff6a2a17dbf9ad30a6bde2bd6aadb5bb0
pavinane <pavimegan77@gmail.com>
2024-08-27 07:34:53 +0530

    create header with active menus

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,19 +1,53 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
+const MenuList = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "About",
+    link: "/about",
+  },
+  {
+    name: "Our Solution",
+    link: "/oursolution",
+  },
+  {
+    name: "Career",
+    link: "/career", // Fixed typo here (was "/carrer")
+  },
+];
 
 function Header() {
+  const location = useLocation();
+
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to={`/`}>Home</Link>
-          </li>
-          <li>
-            <Link to={`about`}>About</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="flex justify-between p-4 px-6 items-center">
+      <div className="log">
+        <h1 className="font-bold text-2xl">Spline Craft</h1>
+      </div>
+      <div className="menus flex justify-between gap-10 items-center">
+        {MenuList?.map((item) => {
+          const isActive = location.pathname === item.link;
+          return (
+            <div className="menu_tit" key={item.name}>
+              <Link
+                to={item.link}
+                className={`${
+                  isActive ? "text-blue-500" : "text-black"
+                } hover:text-blue-500`}
+              >
+                {item.name}
+              </Link>
+            </div>
+          );
+        })}
+        <button className="bg-[#5D5A88] hover:bg-indigo-700 text-white font-medium py-2 px-4 rounded-full md:px-6 lg:px-4 text-xs">
+          Get a Quote
+        </button>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      xs: "480px", // Extra Small screens
+      sm: "640px", // Small screens
+      md: "768px", // Medium screens
+      lg: "1024px", // Large screens
+      xl: "1280px", // Extra Large screens
+      "2xl": "1536px", // 2X Extra Large screens
+      "3xl": "1920px",
+    },
   },
   plugins: [],
 };
